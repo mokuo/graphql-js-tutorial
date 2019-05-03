4d9a2c954efec1c7cc5d09177c4bd7b2c3e67c07
mokuo <tennis10988.yk@gmail.com>
2019-05-03 19:58:27 +0900

    Basic Types
    - GraphQL.js Tutorial https://graphql.org/graphql-js/basic-types/

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ const { buildSchema } = require('graphql')
 
 const schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `)
 
 const root = {
-  hello: () => {
-    return 'Hello world!'
-  }
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
+  },
+  random: () => {
+    return Math.random()
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6))
+  },
 }
 
 const app = express()
