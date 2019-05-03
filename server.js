00dac38128980e274d9f9cccd5a093382d624b85
mokuo <tennis10988.yk@gmail.com>
2019-05-03 22:53:48 +0900

    Mutations and Input Types | GraphQL.js Tutorial
    - https://graphql.org/graphql-js/mutations-and-input-types/

--- server.js
+++ server.js
@@ -1,48 +1,65 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const { buildSchema } = require('graphql');
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
   }
 
   type Query {
-    getDie(numSides: Int): RandomDie
-  }
-`);
-
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides;
+    getMessage(id: ID!): Message
   }
 
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides);
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }
+`)
 
-  roll({ numRolls }) {
-    const output = [];
-    for (let i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce());
-    }
-    return output;
+class Message {
+  constructor(id, { content, author }) {
+    this.id = id
+    this.content = content
+    this.author = author
   }
 }
+
+const fakeDatabase = {}
 
 const root = {
-  getDie: function ({ numSides }) {
-    return new RandomDie(numSides || 6);
-  }
+  getMessage: ({ id }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id' + id)
+    }
+    return new Message(id, fakeDatabase[id])
+  },
+  createMessage: ({ input }) => {
+    const id = require('crypto').randomBytes(10).toString('hex')
+    fakeDatabase[id] = input
+    return new Message(id, input)
+  },
+  updateMessage: ({ id, input }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id' + id)
+    }
+    fakeDatabase[id] = input
+    return new Message(id, input)
+  },
 }
 
-const app = express();
+const app = express()
 app.use('/graphql', graphqlHTTP({
   schema,
   rootValue: root,
   graphiql: true,
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+}))
+app.listen(4000)
+console.log('Running a GraphQL API server at localhost:4000/graphql')
