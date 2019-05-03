8805a49f1d601c77f299e6b24bd4e759b72f73ed
mokuo <tennis10988.yk@gmail.com>
2019-05-03 20:52:31 +0900

    Object Types | GraphQL.js Tutorial
    - https://graphql.org/graphql-js/object-types/

--- server.js
+++ server.js
@@ -1,28 +1,48 @@
-const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const { buildSchema } = require('graphql')
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
-  type Query {
-    rollDice(numDice: Int!, numSides: Int): [Int]
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
   }
-`)
 
-const root = {
-  rollDice: ({ numDice, numSides }) => {
-    const output = []
-    for (let i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+  type Query {
+    getDie(numSides: Int): RandomDie
+  }
+`);
+
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({ numRolls }) {
+    const output = [];
+    for (let i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
     }
-    return output
-  },
+    return output;
+  }
 }
 
-const app = express()
+const root = {
+  getDie: function ({ numSides }) {
+    return new RandomDie(numSides || 6);
+  }
+}
+
+const app = express();
 app.use('/graphql', graphqlHTTP({
   schema,
   rootValue: root,
   graphiql: true,
-}))
-app.listen(4000)
-console.log('Running a GraphQL API server at localhost:4000/graphql')
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
